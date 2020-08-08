e12c2d49416ee1fca74cd5677f9f7cb8e9c84cd9
Madolyn <34144339+madolyn02@users.noreply.github.com>
2020-08-08 02:48:06 -0400

    Start of something new (and responsive)!

--- js/index.js
+++ js/index.js
@@ -1,46 +1,46 @@
-function includeHTML() { //Thanks w3!
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-};
-
-// Change the selector if needed
-var $table = $('table.scroll'),
-    $bodyCells = $table.find('tbody tr:first').children(),
-    colWidth;
-
-// Adjust the width of thead cells when window resizes
-$(window).resize(function() {
-    // Get the tbody columns width array
-    colWidth = $bodyCells.map(function() {
-        return $(this).width();
-    }).get();
-
-    // Set the width of thead columns
-    $table.find('thead tr').children().each(function(i, v) {
-        $(v).width(colWidth[i]);
-    });
-}).resize(); // Trigger resize handler
-© 2020 GitHub, Inc.
+// function includeHTML() { //Thanks w3!
+//   var z, i, elmnt, file, xhttp;
+//   /*loop through a collection of all HTML elements:*/
+//   z = document.getElementsByTagName("*");
+//   for (i = 0; i < z.length; i++) {
+//     elmnt = z[i];
+//     /*search for elements with a certain atrribute:*/
+//     file = elmnt.getAttribute("include-html");
+//     if (file) {
+//       /*make an HTTP request using the attribute value as the file name:*/
+//       xhttp = new XMLHttpRequest();
+//       xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4) {
+//           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+//           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+//           /*remove the attribute, and call this function once more:*/
+//           elmnt.removeAttribute("include-html");
+//           includeHTML();
+//         }
+//       }
+//       xhttp.open("GET", file, true);
+//       xhttp.send();
+//       /*exit the function:*/
+//       return;
+//     }
+//   }
+// };
+//
+// // Change the selector if needed
+// var $table = $('table.scroll'),
+//     $bodyCells = $table.find('tbody tr:first').children(),
+//     colWidth;
+//
+// // Adjust the width of thead cells when window resizes
+// $(window).resize(function() {
+//     // Get the tbody columns width array
+//     colWidth = $bodyCells.map(function() {
+//         return $(this).width();
+//     }).get();
+//
+//     // Set the width of thead columns
+//     $table.find('thead tr').children().each(function(i, v) {
+//         $(v).width(colWidth[i]);
+//     });
+// }).resize(); // Trigger resize handler
+// © 2020 GitHub, Inc.
